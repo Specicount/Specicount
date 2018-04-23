09cf703b2d7d29d61dc122e165c6562734e5f32e
Elliott George Wagener <u6053577@anu.edu.au>
2018-04-24 05:02:04 +1000

    Made image hover jquery code global

--- scripts.php
+++ scripts.php
@@ -4,3 +4,33 @@
 <script src="js/popper.js"></script>
 <script src="js/bsadmin.js"></script>
 <script src="js/fontawesome-all.js"></script>
+<script>
+$(document).ready(function() {
+    // This uses the hoverIntent jquery plugin to avoid excessive queuing of animations
+    // If mouse intends to hover over specimen
+    $(".specimen-container").hoverIntent(
+        function() {
+            var spec_id = $(this).attr('id');
+            fadeInOverlay(spec_id);
+        },
+        function() {
+            var spec_id = $(this).attr('id');
+            fadeOutOverlay(spec_id);
+        });
+
+    function fadeInOverlay(spec_id) {
+        $("#"+spec_id+"_overlay").fadeIn(200);
+        $("#"+spec_id+"_counter").fadeOut(200);
+    }
+
+    function fadeOutOverlay(spec_id) {
+        $("#"+spec_id+"_overlay").fadeOut(200);
+        $("#"+spec_id+"_counter").fadeIn(200);
+    }
+
+    //If close button on overlay clicked
+    $(".overlay .close-btn").click(function() {
+        var spec_id = $(this).attr('id').split("_")[0];
+        fadeOutOverlay(spec_id);
+    })
+</script>
