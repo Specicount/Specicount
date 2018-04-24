28d68ddd7452e60cb00cc1d6b635be09bf2da558
Elliott George Wagener <u6053577@anu.edu.au>
2018-04-24 17:36:33 +1000

    Fixed specimen IDs

--- sample.php
+++ sample.php
@@ -21,6 +21,7 @@ $core = $_GET["core"];
 $sample = $_GET["sample"];
 
 // TODO plant_function_type
+// Plan Functional Group
 
 /* =============================================
     validation if posted
@@ -41,8 +42,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && Form::testToken('add-new-found-sampl
         }
         header("Refresh:0");
         exit;
+    } else if ($_POST['delete-from-sample']) {
+        $db->deleteRows('found_specimen', array('spec_id' => Mysql::SQLValue($_POST['delete-from-sample'])));
+        if($db->error()) {
+            $msg = '<p class="alert alert-danger">'.$_POST['delete-from-sample'].' could not be deleted !</p>' . " \n";
+        } else {
+            $msg = '<p class="alert alert-success">'.$_POST['delete-from-sample'].' deleted successfully !</p>' . " \n";
+        }
     } else {
-
 
         $db->selectRows('samples', array('sample_id' => Mysql::SQLValue($sample)), null, null, true, 1);
         $sample_data = $db->recordsArray()[0];
@@ -52,6 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && Form::testToken('add-new-found-sampl
             $sub_post = array_slice($_POST, 4);
             foreach ($sub_post as $specimen => $count) {
                 if ($specimen != "lycopodium" && $specimen != "charcoal" && $specimen != "last_edit") {
+                    $specimen = trim(base64_decode(str_replace("-", "=", $specimen)));
                     $update = array();
                     $update["spec_id"] = Mysql::SQLValue($specimen);
                     $update["sample_id"] = Mysql::SQLValue($sample);
@@ -177,29 +185,28 @@ if($db->rowCount() > 0) {
     $form->addHtml('<div class="square-grid">');
 
     foreach ($specs as $specimen) {
+        $specimen_name = $specimen["spec_id"];
+        $specimen["spec_id"] = str_replace("=", "-", trim(base64_encode($specimen["spec_id"])));
         $image = $specimen["image_folder"].$specimen["primary_image"];
-        $form->addHtml('<div id="'.$specimen["spec_id"].'" class="specimen-container cell"');
+        $form->addHtml('<div id="'.$specimen["spec_id"].'_container" class="specimen-container cell"');
         if (is_file($image)) {
-            $form->addHtml(' style="background-image:url(\'/phpformbuilder/images/uploads/'.$specimen["spec_id"].'/'.$specimen["primary_image"].'\');"');
+            $form->addHtml(' style="background-image:url(\'/phpformbuilder/images/uploads/'.$specimen_name.'/'.$specimen["primary_image"].'\');"');
         }
         $form->addHtml('>');
         $form->addHtml('<div id="'.$specimen["spec_id"].'_counter" class="counter"><p id="'.$specimen["spec_id"].'_counter_text">' . $specimen["count"] . '</p></div>');
         $form->addHtml('<div id="'.$specimen["spec_id"].'_overlay" class="overlay">');
-        $form->addHtml('<text>ID: ' . $specimen["spec_id"] . '</text>');
-        $form->addHtml('<a href="add_new_specimen.php?project='.$project.'&core='.$core.'&sample='.$sample.'&edit='.$specimen["spec_id"].'" target="_blank"><i class="fa fa-edit edit-btn"></i></a>');
-        $form->addHtml('<a href="specimen_details.php?spec_id=\'.$specimen["spec_id"].\'" target="_blank"><i class="fa fa-info-circle info-btn"></i></a>');
+        $form->addHtml('<text>ID: ' . $specimen_name . '</text>');
+        $form->addHtml('<a href="add_new_specimen.php?project='.$project.'&core='.$core.'&sample='.$sample.'&edit='.$specimen_name.'" target="_blank"><i class="fa fa-edit edit-btn"></i></a>');
+        $form->addHtml('<a href="specimen_details.php?spec_id='.$specimen_name.'" target="_blank"><i class="fa fa-info-circle info-btn"></i></a>');
         $form->addHtml('<a href="#"><span><i id="'.$specimen["spec_id"].'_close" class="fas fa-window-close close-btn"></i></span></a>');
         $form->addBtn('button', 'add-to-count', 1, '<i class="fa fa-plus"></i>', 'class=btn btn-success add-btn, data-style=zoom-in, onclick=add(\''.$specimen["spec_id"].'\');updateCounter(\''.$specimen["spec_id"].'\')');
-        $form->addBtn('???delete???', 'delete-from-sample', $specimen["spec_id"], ' <i class="fa fa-trash"></i>', 'class=btn btn-danger ladda-button del-btn, data-style=zoom-in, onclick=confirm(\'Are you sure you want to delete this specimen from the sample?\')');
-        $form->addInput('number', $specimen["spec_id"].'_input', $specimen["count"], '', 'required onchange=updateCounter(\''.$specimen["spec_id"].'\')');
+        $form->addBtn('submit', 'delete-from-sample', $specimen["spec_id"], ' <i class="fa fa-trash"></i>', 'class=btn btn-danger ladda-button del-btn, data-style=zoom-in, onclick=confirm(\'Are you sure you want to delete this specimen from the sample?\')');
+        $form->addInput('number', $specimen["spec_id"], $specimen["count"], '', 'required onchange=updateCounter(\''.$specimen["spec_id"].'\')');
         $form->addHtml('</div>');
         $form->addHtml('</div>');
 
     }
     $form->addHtml('</div><br><br>');
-
-
-
 
 } else {
     $form->addHtml('<p style="font-style: italic">No specimens added to this sample</p>');
@@ -219,25 +226,29 @@ require_once "add_form_html.php";
         }
     }
 
+    // Note: the html input element has id=spec_id
     function updateCounter(spec_id){
-        document.getElementById(spec_id+"_counter_text").innerHTML = document.getElementById(spec_id+"_input").value;
+
+        document.getElementById(spec_id+"_counter_text").innerHTML = document.getElementById(spec_id).value;
     }
+
     function add(spec_id) {
-        document.getElementById(spec_id+"_input").value = parseFloat(document.getElementById(spec_id+"_input").value) + 1;
+        document.getElementById(spec_id).value = parseFloat(document.getElementById(spec_id).value) + 1;
     }
 
     function subtract(spec_id){
-        document.getElementById(spec_id+"_input").value = parseFloat(document.getElementById(spec_id+"_input").value) - 1;
+        document.getElementById(spec_id).value = parseFloat(document.getElementById(spec_id).value) - 1;
     }
     window.onkeyup = function(e) {
         var key = e.keyCode ? e.keyCode : e.which;
-
         <?php
         $keys = str_split("qwertyuiopasdfghjklzxcvbnm"); // hotkeys
         $i = 0;
         foreach ($keys as $k) {
+            $spec_id = str_replace("=", "-", trim(base64_encode($specs[$i]["spec_id"])));
             echo "if (key == ".(ord($k) - 32).") {
-                   document.getElementById('".$specs[$i]["spec_id"]."').value = parseFloat(document.getElementById('".$specs[$i]["spec_id"]."').value) + 1;
+                    add('".$spec_id."');
+                    updateCounter('".$spec_id."');
                   }";
             $i++;
             if ($i >= count($specs)) break;
