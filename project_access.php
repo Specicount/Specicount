ef494560d2cf24c02e42880089faa58150a499bb
Elliott George Wagener <u6053577@anu.edu.au>
2018-09-22 12:29:40 +1000

    Started progress on changing project_access.php

--- project_access.php
+++ project_access.php
@@ -71,16 +71,16 @@ $form->setOptions(array('buttonWrapper'=>'')); // So that the button can be prin
 $form->startFieldset('Add New User to Project');
 
 $form->setCols(0,5);
-$form->groupInputs('username', 'access_level', 'submit-btn');
-$form->addHelper('Username', 'username');
-$form->addInput('text', 'username', '', '', '');
+$form->groupInputs('new_email', 'new_access_level', 'submit-btn');
+$form->addHelper('Email', 'new_email');
+$form->addInput('text', 'new_email');
 
 $form->setCols(0,2);
-$form->addHelper('Access Level', 'access_level');
-$form->addOption('access_level', 'visitor', 'Visitor', '', '');
-$form->addOption('access_level', 'collaborator', 'Collaborator', '', '');
-$form->addOption('access_level', 'admin', 'Admin', '', '');
-$form->addSelect('access_level', '', '');
+$form->addHelper('Access Level', 'new_access_level');
+$form->addOption('new_access_level', 'visitor', 'Visitor');
+$form->addOption('new_access_level', 'collaborator', 'Collaborator');
+$form->addOption('new_access_level', 'admin', 'Admin');
+$form->addSelect('new_access_level');
 
 $form->setCols(0,5);
 $form->addBtn('submit', 'submit-btn', "save", '<i class="fa fa-plus" aria-hidden="true"></i> Add New User', 'class=btn btn-success ladda-button, data-style=zoom-in', 'add-group');
@@ -92,31 +92,57 @@ $form->startFieldset('Edit Current Users');
 
 $filter = $form->getFilterArray();
 $db = new Mysql();
-$db->selectRows($form->getTableName(), $filter);
+$sql =  "SELECT email, first_name, last_name, access_level FROM users NATURAL JOIN user_project_access ".
+        Mysql::buildSQLWhereClause($filter).
+        " ORDER BY access_level";
+$db->query($sql);
+$i = 0;
+print_r($db->error());
+print_r($db->recordsArray());
+print_r($_POST);
 foreach ($db->recordsArray() as $user) {
-    $username_input = 'username,'.$user["username"];
-    $access_input = 'access_level,'.$user["username"];
     $form->startFieldset('');
 
-    $form->setCols(0,5);
-    $form->groupInputs($username_input, $access_input, "submit-btn");
-    $form->addHelper('Username', $username_input);
-    $form->addInput('text', $username_input, $user['username'], '', 'readonly="readonly"');
+
+    $form->setCols(0,3);
+    $form->groupInputs("email[]", "first_name[]", "last_name[]", "submit-btn");
+    $form->addInput("hidden", "email[]", $user["email"]);
+    $form->addInput("text", "first_name[]", $user["first_name"], '', 'readonly="readonly"');
+    $form->addInput("text", "last_name[]", $user["last_name"], '', 'readonly="readonly"');
+    $form->addHelper('Access Level', "access_level[]");
 
     $form->setCols(0,2);
-    $_SESSION[$form->getFormName()][$access_input] = $user['access_level']; // Fill in access level from db
-    $form->addHelper('Access Level', $access_input);
-    $form->addOption($access_input, 'visitor', 'Visitor', '', '');
-    $form->addOption($access_input, 'collaborator', 'Collaborator', '', '');
-    $form->addOption($access_input, 'admin', 'Admin', '', '');
-    $form->addSelect($access_input, '', '');
+    if ($user["access_level"] === "owner") {
+        $form->addInput("text", "access_level[]", ucwords($user["access_level"]), '', 'readonly="readonly"');
+    } else {
+        $form->addOption("access_level[]", 'visitor', 'Visitor', '', '');
+        $form->addOption("access_level[]", 'collaborator', 'Collaborator', '', '');
+        $form->addOption("access_level[]", 'admin', 'Admin', '', '');
+        $form->addSelect("access_level[]");
+    }
 
 
-    $form->setCols(0,5);
-    $form->addBtn('submit', 'delete-btn', $user['username'], '<i class="fa fa-trash" aria-hidden="true"></i> Remove User', 'class=btn btn-danger, onclick=return confirm(\'Are you sure you want to remove this user from your project?\')', 'delete-btn-'.$user["username"]);
-    $form->printBtnGroup('delete-btn-'.$user["username"]);
+
+//
+//    $form->addInput("text", $email_input, $user["email"]);
+//    $form->addHelper('Username', $username_input);
+//    $form->addInput('text', $username_input, $user['username'], '', 'readonly="readonly"');
+//
+//    $form->setCols(0,2);
+//    $_SESSION[$form->getFormName()]["access_level"][$i] = $user['access_level']; // Fill in access level from db
+//    $form->addHelper('Access Level', $access_input);
+//    $form->addOption($access_input, 'visitor', 'Visitor', '', '');
+//    $form->addOption($access_input, 'collaborator', 'Collaborator', '', '');
+//    $form->addOption($access_input, 'admin', 'Admin', '', '');
+//    $form->addSelect($access_input, '', '');
+
+
+    $form->setCols(0,4);
+    $form->addBtn('submit', 'delete-btn[]', $user['email'], '<i class="fa fa-trash" aria-hidden="true"></i> Remove User', 'class=btn btn-danger, onclick=return confirm(\'Are you sure you want to remove this user from your project?\')', 'delete-btn-'.$user["email"]);
+    $form->printBtnGroup('delete-btn-'.$user["email"]);
 
     $form->endFieldset();
+    $i++;
 }
 
 $form->endFieldset();
