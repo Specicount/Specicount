9f47fed6c33ca4c7744a1f39b4a181751909568e
Elliott George Wagener <u6053577@anu.edu.au>
2018-10-04 19:08:41 +1000

    Made the trendline a log function

--- concentration.php
+++ concentration.php
@@ -11,32 +11,65 @@ use phpformbuilder\database\Mysql;
 $filter = $form->getFilterArray();
 $db->selectRows("concentration_curve", $filter);
 
-$result = array();
+$XY_data = array();
+$X = array();
+$Y = array();
 if (!$db->error()) {
     $curve_data = $db->recordsArray();
-    $result[] = '["Tally Count", "Data Point"]';
     foreach ($curve_data as $point) {
-        $result[] = '[' . $point["tally_count"] . ', ' . $point["unique_spec"] . ']';
-
+        $X[] = $point["tally_count"];
+        $Y[] = $point["unique_spec"];
+        $XY_data[] = '[' . $point["tally_count"] . ', ' . $point["unique_spec"] . ', null]';
     }
 }
+$XY_data_string = implode($XY_data,", ");
 
-$result_concentration = implode($result,", ");
 if ($db->rowCount() > 0) {
+    // Create log trendline
+    $logX = array_map('log', $X);
+    $n = count($X);
+    $square = function ($x) {return pow($x,2);};
+    $multiply = function($x,$y) {return $x*$y;};
+    $x_squared = array_sum(array_map($square, $logX));
+    $xy = array_sum(array_map($multiply, $logX, $Y));
+
+    // Log function of the form $aFit*log(x)+$bFit
+    $bFit = ($n * $xy - array_sum($Y) * array_sum($logX)) /
+        ($n * $x_squared - pow(array_sum($logX), 2));
+    $aFit = (array_sum($Y) - $bFit * array_sum($logX)) / $n;
+
+    // Create an array of of XY values that will represent the log trendline
+    $num_points = end($X); // The x value of the last data point on the plot
+    for ($i=1; $i<=$num_points; $i+=0.5) {
+        $log_data[] = '[' . $i . ', null, ' . ($aFit*log($i)+$bFit) . ']';
+    }
+    $log_data_string = implode($log_data,", ");
+
     echo "
         <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
         <script>
-        google.charts.load('current', {packages: ['corechart', 'bar']});
+        google.charts.load('current', {packages: ['corechart']});
         google.charts.setOnLoadCallback(drawBasic_concentration);
         
         function drawBasic_concentration() {
         
-            var data = google.visualization.arrayToDataTable([
-                $result_concentration
-            ]);
+            var data = new google.visualization.DataTable();
+            data.addColumn('number','x');
+            data.addColumn('number','y');
+            data.addColumn('number','trendline');
+            data.addRows([$XY_data_string]);
+            data.addRows([$log_data_string]);
+
             
-            var options = {
+            var options = {                
                 title:'Concentration Curve',
+                seriesType:'scatter',
+                series: {
+                    1: {
+                        type: 'line'
+                    }
+                },
+                colors: ['#4286f4','#9bc1ff'],
                 chartArea: {width:300},
                 hAxis: {
                   title: 'Tally Count'
@@ -44,20 +77,14 @@ if ($db->rowCount() > 0) {
                 vAxis: {
                   title: 'Unique Specimens'
                 },
-                legend: 'none',
-                trendlines: {
-                  0: {
-                    type: 'exponential',
-                    visibleInLegend: true,
-                  }
-                }
+                legend: 'none'
             };
             
-            var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+            var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
             
             chart.draw(data, options);
             
-                    var ref = $(\"button[name='stats-btn']\");
+            var ref = $(\"button[name='stats-btn']\");
             var popup = $(\"#chart_div\");
             var popper = new Popper(ref, popup, {
                 placement: 'bottom'
