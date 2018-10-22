4fcc177d0dbef71da58ffd557b0dcb8beb68152a
Elliott George Wagener <u6053577@anu.edu.au>
2018-10-22 17:39:31 +1100

    Implemented the update function for editing user details

--- register.php
+++ register.php
@@ -20,20 +20,29 @@ class Register_Form extends Post_Form {
     }
 
     protected function additionalValidation() {
-        $this->validator->email()->validate('email');
-        $this->validator->hasUppercase()->hasLowercase()->hasNumber()->minLength(8)->validate('password');
-        $this->validator->matches('password')->validate('password_conf');
-        $this->validator->recaptcha($_POST["captcha_code"], 'Recaptcha Error')->validate('g-recaptcha-response');
+        if ($this->post_actions["valid"]["create"] == true) {
+            $this->validator->email()->validate('email');
+            $this->validator->hasUppercase()->hasLowercase()->hasNumber()->minLength(8)->validate('password');
+            $this->validator->matches('password')->validate('password_conf');
+            $this->validator->recaptcha($_POST["captcha_code"], 'Recaptcha Error')->validate('g-recaptcha-response');
+        }
         return true;
+    }
+
+    protected function update() {
+        $filter = array();
+        $filter["email"] = Mysql::sqlValue($_SESSION["email"]);
+        $update = array();
+        $update["first_name"] = Mysql::sqlValue($_POST["first_name"]);
+        $update["last_name"] = Mysql::sqlValue($_POST["last_name"]);
+        $update["institution"] = Mysql::sqlValue($_POST["institution"]);
+        $this->db->updateRows("users", $update, $filter);
+        storeDbMsg($this->db,"Successfully updated details!");
     }
 
     protected function create() {
         $this->db->insertRow($this->table_name, $this->update);
         storeDbMsg($this->db,'User: ' . $_POST["email"] . ' added successfully!', "That email already exists!");
-    }
-
-    protected function update() {
-        $this->raiseNotImplemented();
     }
 
     protected function fillFormWithDbValues($record_array) {
@@ -50,24 +59,28 @@ $form = new Register_Form("register","users", 'horizontal', 'novalidate', 'bs4')
 
 $form->setCols(3, 9);
 
-$form->addInput('email', 'email', '', 'Email', 'required, class=col-5');
+if (!isset($_GET["edit"])) {
+    $form->addInput('email', 'email', '', 'Email', 'required, class=col-5');
+}
 $form->addInput('text', 'first_name', '', 'First Name', 'required, class=col-5');
 $form->addInput('text', 'last_name', '', 'Last Name', 'required, class=col-5');
 $form->addInput('text', 'institution', '', 'Your Institution/Company', "class=col-5");
 
 unset($_SESSION[$form->getFormName()]["password"]);
 unset($_SESSION[$form->getFormName()]["password_conf"]);
-$form->addHelper("Must contain atleast 1 number, 1 uppercase letter, 1 lowercase letter", "password");
-$form->addInput('password', 'password', '', 'Password', 'required, class=col-5,
+if (!isset($_GET["edit"])) {
+    $form->addHelper("Must contain atleast 1 number, 1 uppercase letter, 1 lowercase letter", "password");
+    $form->addInput('password', 'password', '', 'Password', 'required, class=col-5,
                 data-fv-stringlength, data-fv-stringlength-min=8, data-fv-stringlength-message=Your password must be at least 8 characters long');
-$form->addInput('password', 'password_conf', '', 'Password Confirmation', 'required, class=col-5,
+    $form->addInput('password', 'password_conf', '', 'Password Confirmation', 'required, class=col-5,
                 data-fv-stringlength, data-fv-stringlength-min=8, data-fv-stringlength-message=Your password must be at least 8 characters long');
 
-$form->addRecaptcha('6Ley73EUAAAAAGlW8U8cgkYJ6k7fIDbTF5Am47Qj', 'recaptcha2', true);
-
-$form->addBtn('submit', 'submit-btn', "save", '<i class="fa fa-user-plus" aria-hidden="true"></i> Register', 'class=btn btn-success ladda-button, data-style=zoom-in', 'my-btn-group');
+    $form->addRecaptcha('6Ley73EUAAAAAGlW8U8cgkYJ6k7fIDbTF5Am47Qj', 'recaptcha2', true);
+}
 if ($_GET["edit"]) {
-    $form->addBtn('submit', 'delete-btn', "delete", '<i class="fa fa-trash" aria-hidden="true"></i> Delete', 'class=btn btn-danger, onclick=return confirm(\'Are you sure you want to delete this core?\')', 'my-btn-group');
+    $form->addBtn('submit', 'submit-btn', "save", '<i class="fa fa-save" aria-hidden="true"></i> Save', 'class=btn btn-success ladda-button, data-style=zoom-in', 'my-btn-group');
+} else {
+    $form->addBtn('submit', 'submit-btn', "save", '<i class="fa fa-user-plus" aria-hidden="true"></i> Register', 'class=btn btn-success ladda-button, data-style=zoom-in', 'my-btn-group');
 }
 $form->printBtnGroup('my-btn-group');
 
@@ -80,6 +93,10 @@ $form->addPlugin("formvalidation","#".$form->getFormName(), "bs4");
 // Render Page
 $page_render = new \classes\Page_Renderer();
 $page_render->setForm($form);
-$page_render->setPageRestrictions(false);
+if ($_GET["edit"]) {
+    $page_render->setPageRestrictions(true);
+} else {
+    $page_render->setPageRestrictions(false);
+}
 $page_render->disableSidebar();
 $page_render->renderPage();
